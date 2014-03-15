b04a78812616472f2e54749b38721e800214ea40
Christophe Mehay <cmehay@student.42.fr>
2014-03-15 20:48:25 +0100

    wip

--- srcs/draw_cone.c
+++ srcs/draw_cone.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/02 03:09:31 by sde-segu          #+#    #+#             */
-/*   Updated: 2014/03/15 17:17:14 by dcouly           ###   ########.fr       */
+/*   Updated: 2014/03/15 20:46:09 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,45 @@
 void	rt_cone(t_env *e, t_data *scene)
 {
 	float	mem;
-	float	vx;
-	float	vz;
-	float	vy;
-	float	ox;
-	float	oz;
-	float	oy;
+	t_pos	v;
+	t_pos	o;
 
-	ox = e->cam.x - scene->pos.x;
-	oz = e->cam.z - scene->pos.z;
-	oy = e->cam.y - scene->pos.y;
-	vx = e->vect.x;
-	vz = e->vect.z;
-	vy = e->vect.y;
+	o.x = e->cam.x - scene->pos.x;
+	o.z = e->cam.z - scene->pos.z;
+	o.y = e->cam.y - scene->pos.y;
+	v.x = e->vect.x;
+	v.z = e->vect.z;
+	v.y = e->vect.y;
 
 	if (scene->angle.y)
 	{
 		mem = e->vect.x;
-		vx = cos(scene->angle.y * M_PI / 180) * mem - sin(scene->angle.y * M_PI     / 180) * e->vect.z;
-		vz = sin(scene->angle.y * M_PI / 180) * mem + cos(scene->angle.y * M_PI     / 180) * e->vect.z;
-		mem = ox;
-		ox = cos(scene->angle.y * M_PI / 180) * mem - sin(scene->angle.y * M_PI     / 180) * oz;
-		oz = sin(scene->angle.y * M_PI / 180) * mem + cos(scene->angle.y * M_PI     / 180) * oz;
+		v.x = cos(scene->angle.y * M_PI / 180) * mem - sin(scene->angle.y * M_PI     / 180) * e->vect.z;
+		v.z = sin(scene->angle.y * M_PI / 180) * mem + cos(scene->angle.y * M_PI     / 180) * e->vect.z;
+		mem = o.x;
+		o.x = cos(scene->angle.y * M_PI / 180) * mem - sin(scene->angle.y * M_PI     / 180) * o.z;
+		o.z = sin(scene->angle.y * M_PI / 180) * mem + cos(scene->angle.y * M_PI     / 180) * o.z;
 	}
 	if (scene->angle.x)
 	{
-		mem = vy;
-		vy = cos(scene->angle.x * M_PI / 180) * mem + sin(scene->angle.x * M_PI     / 180) * vz;
-		vz = sin(scene->angle.x * M_PI / 180) * mem * -1 + cos(scene->angle.x *     M_PI / 180) * vz;
-		mem = oy;
-		oy = cos(scene->angle.x * M_PI / 180) * mem + sin(scene->angle.x * M_PI     / 180) * oz;
-		oz = sin(scene->angle.x * M_PI / 180) * mem * -1 + cos(scene->angle.x *     M_PI / 180) * oz;
+		mem = v.y;
+		v.y = cos(scene->angle.x * M_PI / 180) * mem + sin(scene->angle.x * M_PI     / 180) * v.z;
+		v.z = sin(scene->angle.x * M_PI / 180) * mem * -1 + cos(scene->angle.x *     M_PI / 180) * v.z;
+		mem = o.y;
+		o.y = cos(scene->angle.x * M_PI / 180) * mem + sin(scene->angle.x * M_PI     / 180) * o.z;
+		o.z = sin(scene->angle.x * M_PI / 180) * mem * -1 + cos(scene->angle.x *     M_PI / 180) * o.z;
 	}
-	ox = scene->pos.x + ox;
-	oy = scene->pos.y + oy;
-	oz = scene->pos.z + oz;
-	e->a = pow((vx), 2) + pow((vy), 2)
-		- pow((vz), 2) * pow(scene->radius, 2) / 100;
-	e->b = 2 * ((vx) * (ox - scene->pos.x)
-			+ vy * (oy - scene->pos.y)
-			- (vz) * (oz - scene->pos.z) * pow(scene->radius, 2) / 100);
-	e->c = pow((ox - scene->pos.x), 2)
-		+ pow((oy - scene->pos.y), 2)
-		- pow((oz - scene->pos.z), 2) * pow(scene->radius, 2) / 100;
+	o.x = scene->pos.x + o.x;
+	o.y = scene->pos.y + o.y;
+	o.z = scene->pos.z + o.z;
+	e->a = pow((v.x), 2) + pow((v.y), 2)
+		- pow((v.z), 2) * pow(scene->radius, 2) / 100;
+	e->b = 2 * ((v.x) * (o.x - scene->pos.x)
+			+ v.y * (o.y - scene->pos.y)
+			- (v.z) * (o.z - scene->pos.z) * pow(scene->radius, 2) / 100);
+	e->c = pow((o.x - scene->pos.x), 2)
+		+ pow((o.y - scene->pos.y), 2)
+		- pow((o.z - scene->pos.z), 2) * pow(scene->radius, 2) / 100;
 	e->ray.delta = pow(e->b, 2) - (4 * e->a * e->c);
 	get_cone_to_print(e, scene);
 }
